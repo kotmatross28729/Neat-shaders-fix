90f855bdbf0db29d51c299e2863cee1c0b920bb0
kotmatross28729 <kotmatross228@gmail.com>
2024-03-16 17:08:00 +0300

    Fix crash on server

--- src/main/java/vazkii/neat/Neat.java
+++ src/main/java/vazkii/neat/Neat.java
@@ -1,5 +1,6 @@
 package vazkii.neat;
 
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -7,16 +8,18 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Neat.MOD_ID, name = Neat.MOD_NAME, version = Neat.VERSION)
 public class Neat {
-	
+
 	public static final String MOD_ID = "Neat";
 	public static final String MOD_NAME = MOD_ID;
 	public static final String BUILD = "GRADLE:BUILD";
-	public static final String VERSION = "GRADLE:VERSION-" + BUILD;	
+	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		NeatConfig.init(event.getSuggestedConfigurationFile());
-		
-		MinecraftForge.EVENT_BUS.register(new HealthBarRenderer());
+        boolean client = FMLLaunchHandler.side().isClient();
+        if(client) {
+            MinecraftForge.EVENT_BUS.register(new HealthBarRenderer());
+        }
 	}
 }
